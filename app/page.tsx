a9487dc6bb61ab7b1af8d6c117206944b888f543
richardybanez037 <richardybanez037@gmail.com>
2025-05-15 18:15:34 +0800

    add - fb link update - headers ui

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,7 @@ export default function Home() {
     '/b_494814074_711796674675800_1916715554277404591_n.jpg',
     '/b_s_494334285_1202180258303455_3402547453332226795_n.jpg',
     '/b_s_494328194_1006288311698056_932510846075985259_n.jpg',
-    '/b_s_494329823_3106490832837996_9169633562725432498_n.jpg',
-
+    '/b_s_494329823_3106490832837996_9169633562725432498_n.jpg'
   ]
   return <div>
     <div className="flex justify-center" id="paragon">
@@ -24,7 +23,7 @@ export default function Home() {
     <nav className="flex justify-center border-4 border-blue-700 border-x-0 h-fit">
       <ul className="flex flex-wrap justify-center">
         <li className="sm:px-3 sm:py-1 px-2 py-1 cursor-pointer duration-200 hover:bg-blue-700 sm:text-xl text-md">
-          <a href="#A">ABOUT</a>
+          <a href="#A">THE TEAM</a>
         </li>
         <li className="sm:px-3 sm:py-1 px-2 py-1 cursor-pointer duration-200 hover:bg-blue-700 sm:text-xl text-md">
           <a href="#T">TOURNAMENTS</a>
@@ -44,22 +43,22 @@ export default function Home() {
           />
         )}
       </Carousel>
-      <div className="absolute bg-linear-to-b from-transparent to-black to-50% flex items-center justify-center p-30 w-full -bottom-40">
+      <div className="absolute bg-linear-to-b from-transparent to-black to-50% flex items-center justify-center p-20 w-full -bottom-25">
         <h1 className="text-5xl sm:text-7xl text-blue-600 font-semibold text-center tracking-widest text-shadow-lg/20">PARAGON<br/>BLADERZ</h1>
       </div>
     </section>
     <section 
-      className="mt-40 flex flex-col justify-center items-center"
+      className="mt-25 flex flex-col justify-center items-center"
       id="A"
     >
-        <h2 className="pl-2 text-3xl text-blue-500 font-semibold my-2">ABOUT</h2>
+        <h2 className="px-5 py-2 text-3xl font-semibold my-2 border-5 border-blue-700">THE TEAM</h2>
         <p>Cebu-based bladers</p>
     </section>
     <section 
       className="mt-40 flex flex-col justify-center items-center"
       id="T"
     >
-        <h2 className="pl-2 text-3xl text-blue-500 font-semibold my-2">TOURNAMENTS</h2>
+        <h2 className="px-5 py-2 text-3xl font-semibold my-2 border-5 border-blue-700">TOURNAMENTS</h2>
         <img 
           src='b_t_494817139_656001597429783_7428790834717624631_n.jpg'
           className="max-w-300 object-contain w-full"
@@ -69,7 +68,7 @@ export default function Home() {
       className="mt-40 flex flex-col justify-center items-center"
       id="G"
     >
-      <h2 className="pl-2 text-3xl text-blue-500 font-semibold my-2">GALLERY</h2>
+      <h2 className="px-5 py-2 text-3xl font-semibold my-2 border-5 border-blue-700">GALLERY</h2>
       <div className="grid grid-cols-2 gap-4 max-w-300 px-4">
         <div className="grid gap-4">
             <img className="h-auto max-w-full rounded-lg" src="/mem_sol_495267989_494908636950121_8940839545545571094_n.jpg" alt=""/>
@@ -92,7 +91,7 @@ export default function Home() {
         <div className="w-full max-w-300 flex justify-between items-center">
           <div className="flex gap-2 items-center">
             <img src='/logo.png' width={60} alt="logo" />
-            <a href="" target="_">
+            <a href="https://www.facebook.com/profile.php?id=61576092004210" target="_">
               <img
                 src='/2023_Facebook_icon.svg'
                 width={40}
@@ -107,7 +106,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="flex justify-center text-sm bg-black p-2 border-1 border-gray-800 border-x-0 border-b-0 text-gray-500">
+      <div className="flex justify-center text-[2.5vw] bg-black p-2 border-1 border-gray-800 border-x-0 border-b-0 text-gray-500 sm:text-sm">
         Copyright &#169; 2025 Paragon Bladerz | Developed by&nbsp;<a href="https://rybanez.vercel.app/" target="_" className="text-blue-900 hover:text-blue-600">Richard Ybañez</a>
       </div>
     </footer>
